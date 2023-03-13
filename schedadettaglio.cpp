b794fcd23370325cecba0429c81d75b7aaf51f5f
Matteo Provincia <taranet@libero.it>
2023-03-13 17:03:20 +0100

    perfezionata schedadettaglio SALVA

--- schedadettaglio.cpp
+++ schedadettaglio.cpp
@@ -17,25 +17,8 @@ SchedaDettaglio::SchedaDettaglio(QString praticaPassata, QSqlDatabase db, QWidge
     if(!db.isOpen()) db.open();
 
     if(db.isOpen()) qInfo() << "aperto";
-/*
-    QSqlQuery *qry;
-    qry = new QSqlQuery(db);
-    qry->prepare("SELECT * FROM Pratiche WHERE Pratica = :pratica");
-    qry->bindValue(":pratica", pratica);
-    if(qry->exec())
-        qInfo() << "eseguita query";
 
-    qry->next();
-    ui->TextPratica->setText(qry->value("Pratica").toString());
-    ui->plainTextTitolo->setPlainText(qry->value("Titolo").toString());
-    ui->textCup->setText(qry->value("Cup").toString());
-
-    db.close();
-*/
     popolaCampi();
-/*
-    popolaTree();
-*/
 
 }
 
@@ -80,14 +63,18 @@ void SchedaDettaglio::popolaCampi()
             QTextEdit *tEdit = new QTextEdit(this);
             tEdit->setText(qryPratica->value(qry->value("NomeColonna").toString()).toString());
             tEdit->setMaximumHeight(60);
+            tEdit->setObjectName(qry->value("NomeColonna").toString());
             //connect(tEdit, &QTextEdit::textChanged, this, &SchedaDettaglio::aggiungiCampoCambiato);
             campi->append(tEdit);
             pubblicaCampo(qry->value("Categoria").toString(), label, tEdit);
         }
-        else if(qry->value("TipoColonna").toString().compare("Testo")==0)
+        else if(qry->value("TipoColonna").toString().compare("Testo")==0 ||
+                qry->value("TipoColonna").toString().compare("Data")==0 ||
+                qry->value("TipoColonna").toString().compare("Decimale")==0)
         {
             QLineEdit *lEdit = new QLineEdit(this);
             lEdit->setText(qryPratica->value(qry->value("NomeColonna").toString()).toString());
+            lEdit->setObjectName(qry->value("NomeColonna").toString());
             //connect(lEdit, &QLineEdit::textChanged, this, &SchedaDettaglio::aggiungiCampoCambiato);
             campi->append(lEdit);
             pubblicaCampo(qry->value("Categoria").toString(), label, lEdit);
@@ -96,25 +83,11 @@ void SchedaDettaglio::popolaCampi()
         {
             QCheckBox *cBox = new QCheckBox();
             cBox->setChecked(qryPratica->value(qry->value("NomeColonna").toString()).toInt());
+            cBox->setObjectName(qry->value("NomeColonna").toString());
             campi->append(cBox);
             pubblicaCampo(qry->value("Categoria").toString(), label, cBox);
         }
-        else if(qry->value("TipoColonna").toString().compare("Decimale")==0)
-        {
-            QLineEdit *nEdit = new QLineEdit(this);
-//            double decimal = qryPratica->value(qry->value("NomeColonna").toString()).toDouble();
-            nEdit->setText(qryPratica->value(qry->value("NomeColonna").toString()).toString());
-            campi->append(nEdit);
-            pubblicaCampo(qry->value("Categoria").toString(), label, nEdit);
-        }
-        else if(qry->value("TipoColonna").toString().compare("Data")==0)
-        {
-            QDateEdit *dEdit = new QDateEdit(this);
-            QString data(qryPratica->value(qry->value("NomeColonna").toString()).toString());
-            dEdit->setDate(QDate::fromString(data));
-            campi->append(dEdit);
-            pubblicaCampo(qry->value("Categoria").toString(), label, dEdit);
-        }
+
     }
     qInfo() << campi[0].data();
     db.close();
@@ -146,27 +119,47 @@ void SchedaDettaglio::on_pushButton_clicked()
 
     if(!db.isOpen()) db.open();
 
-    // QUESTO MI SERVE PER CAPIRE SU COSA FARE IL CASTING
-    QLineEdit *l = qobject_cast<QLineEdit*>(campi->value(0));
-    qInfo() << (campi->value(0)->inherits(QTextEdit::staticMetaObject.className()));
-    qInfo() << l->text();
-    //-----------------------
+    QListIterator<QWidget*> i(*campi);
+    while(i.hasNext())
+    {
+        QWidget *wid = i.next();
+        QTextEdit *tEdit;
+        QLineEdit *lEdit;
+        QCheckBox *cBox;
 
-    /*
-    if(db.isOpen()) qInfo() << "aperto";
+        QString chiave = "";
+        QString valore = "";
 
-    QSqlQuery *qry;
-    qry = new QSqlQuery(db);
-    qry->prepare("UPDATE Pratiche SET Titolo = :titolo, Cup = :cup WHERE Pratica = :pratica");
-    qry->bindValue(":pratica", pratica);
-    qry->bindValue(":titolo", ui->plainTextTitolo->toPlainText());
-    qry->bindValue(":cup", ui->textCup->toPlainText());
-    if(qry->exec())
-        qInfo() << "eseguita query";
+        if(wid->inherits(QLineEdit::staticMetaObject.className()))
+        {
+            lEdit = qobject_cast<QLineEdit*>(wid);
+            chiave = lEdit->objectName();
+            valore = lEdit->text();
+        }
+        if(wid->inherits(QTextEdit::staticMetaObject.className()))
+        {
+            tEdit = qobject_cast<QTextEdit*>(wid);
+            chiave = tEdit->objectName();
+            valore = tEdit->toPlainText();
+        }
+        if(wid->inherits(QCheckBox::staticMetaObject.className()))
+        {
+            cBox = qobject_cast<QCheckBox*>(wid);
+            chiave = cBox->objectName();
+            valore = (cBox->isChecked()?"1":"0");
+        }
 
-    qry->next();
+        if(chiave.compare("Pratica")==0) continue;
 
-*/
+        QSqlQuery *qry;
+        qry = new QSqlQuery(db);
+        qry->prepare("UPDATE Pratiche SET "+ chiave +" = :valore WHERE Pratica = :pratica");
+        qry->bindValue(":valore", valore);
+        qry->bindValue(":pratica", pratica);
+        qry->exec();
+    }
+
     db.close();
+    this->close();
 }
 
